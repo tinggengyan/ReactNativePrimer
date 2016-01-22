fe9188159a4b5b4db171dfc1dca2ef583d55fbc1
SteveYan <tinggengyan@gmail.com>
2016-01-23 02:47:54 +0800

    init commit

---
+++ index.android.js
@@ -0,0 +1,60 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class ReactNativePrimer extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
+        </Text>
+         <Text style={styles.signs}>
+          Hello world!My name is Steve!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  signs: {
+    fontSize: 30,
+    textAlign: 'right',
+    color: '#e91e63',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ReactNativePrimer', () => ReactNativePrimer);
